c8894246cd8c2155136adc22e7edeceab031524c
元方 <luckeast@163.com>
2021-01-10 21:52:58 +0800

    update array.js. 1. [新增]新增数组常用方法indexOf，查找指定元素所在位置，如果没有找到则返回-1 2. [更新]修复distinct问题，当元素是非值类型，如函数类型，则无法去重复 3. [更新]使distinct在高版本浏览器使用高效率的去重复算法

--- array.js
+++ array.js
@@ -22,16 +22,33 @@
 
 	//将数组内元素去重复
 	win.Array.prototype.distinct = function(){
-		var that = this, arr = that.clone(), map = {};
+		var that = this, arr = that.clone();
 		that.clear();
-
-		for(var i = 0; i < arr.length; i++){
-			var item = arr[i];
-			if(!map[item]){
-				map[item] = true;
-				that.push(item);
+		if(win.Set){ //es6以上
+			var map = new win.Set();
+			for(var i = 0; i < arr.length; i++){
+				var item = arr[i];
+				if(!map.has(item)){
+					map.add(item);
+					that.push(item);
+				}
+			}
+		} else { //es5兼容
+			for(var i = 0; i < arr.length; i++){
+				var item = arr[i];
+				if(that.indexOf(item) == -1){
+					that.push(item);
+				}
 			}
 		}
+	}
+
+	//查找指定元素所在位置，如果没有找到则返回-1
+	win.Array.prototype.indexOf = function(item){
+		for(var i = 0; i < this.length; i++){
+			if(this[i] === item) return i;
+		}
+		return -1;
 	}
 
 })(window);
