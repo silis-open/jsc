152c899b548e50cd756f9d8fd0f57f121555b7d6
黄永东 <luckeast@163.com>
2021-01-10 18:15:50 +0800

    update array.js. 1. [新增]清空数组内所有元素 2. [新增]将数组内元素去重复

--- array.js
+++ array.js
@@ -1,5 +1,6 @@
 (function(win){
 
+	//循环数组内元素
 	win.Array.prototype.forEach = function(body, ctx){
 		if(this == null) return;
 		for(var i = 0; i < this.length; i++){
@@ -7,10 +8,30 @@
 		}
 	}
 
+	//将数组内元素，搬到新的数组中，并返回新的数组。
 	win.Array.prototype.clone = function(){
 		var arr = [];
 		for(var i = 0; i < this.length; i++) arr.push(this[i]);
 		return arr;
+	}
+
+	//清空数组内所有元素
+	win.Array.prototype.clear = function(){
+		this.splice(0,this.length);
+	}
+
+	//将数组内元素去重复
+	win.Array.prototype.distinct = function(){
+		var that = this, arr = that.clone(), map = {};
+		that.clear();
+
+		for(var i = 0; i < arr.length; i++){
+			var item = arr[i];
+			if(!map[item]){
+				map[item] = true;
+				that.push(item);
+			}
+		}
 	}
 
 })(window);
