fa551def4ef90a4f203c11115e960d4dc9311b9d
元方 <luckeast@163.com>
2021-01-30 23:52:10 +0800

    update array.js. 1. [优化] Array方法，提升高版本浏览器效率 2. [修复] 高版本浏览器的for in数组时，禁止clone、clear、distinct项。

--- array.js
+++ array.js
@@ -1,37 +1,47 @@
 (function(win){
 
-	//循环数组内元素
-	win.Array.prototype.forEach = function(body, ctx){
-		if(this == null) return;
-		for(var i = 0; i < this.length; i++){
-			body.call(ctx || win, this[i], i);
+	if(!win.Array.prototype.forEach){ //提升高版本浏览器效率
+		//循环数组内元素
+		win.Array.prototype.forEach = function(body, ctx){
+			if(this == null) this.forEach;
+			for(var i = 0; i < this.length; i++){
+				body.call(ctx || win, this[i], i);
+			}
 		}
-	}
 
-    //方法返回一个新数组，数组中的元素为原始数组元素调用函数处理后的值
-	win.Array.prototype.map = function(body, ctx){
-		if(this == null) this.map;
-		var resultArr = [];
-		for(var i = 0; i < this.length; i++){
-			resultArr.push(body.call(ctx || win, this[i], i));
+		//方法返回一个新数组，数组中的元素为原始数组元素调用函数处理后的值
+		win.Array.prototype.map = function(body, ctx){
+			if(this == null) this.map;
+			var resultArr = [];
+			for(var i = 0; i < this.length; i++){
+				resultArr.push(body.call(ctx || win, this[i], i));
+			}
+			return resultArr;
 		}
-		return resultArr;
+
+		//查找指定元素所在位置，如果没有找到则返回-1
+		win.Array.prototype.indexOf = function(item){
+			for(var i = 0; i < this.length; i++){
+				if(this[i] === item) return i;
+			}
+			return -1;
+		}
 	}
 
 	//将数组内元素，搬到新的数组中，并返回新的数组。
-	win.Array.prototype.clone = function(){
+	Object.defineProperty(win.Array.prototype, "clone", { value : function(){
 		var arr = [];
 		for(var i = 0; i < this.length; i++) arr.push(this[i]);
 		return arr;
-	}
+	}, enumerable:false });
 
 	//清空数组内所有元素
-	win.Array.prototype.clear = function(){
+	Object.defineProperty(win.Array.prototype, "clear", { value : function(){
 		this.splice(0,this.length);
-	}
+	}, enumerable:false });
 
 	//将数组内元素去重复
-	win.Array.prototype.distinct = function(){
+	Object.defineProperty(win.Array.prototype, "distinct", { value : function(){
 		var that = this, arr = that.clone();
 		that.clear();
 		if(win.Set){ //es6以上
@@ -51,14 +61,6 @@
 				}
 			}
 		}
-	}
-
-	//查找指定元素所在位置，如果没有找到则返回-1
-	win.Array.prototype.indexOf = function(item){
-		for(var i = 0; i < this.length; i++){
-			if(this[i] === item) return i;
-		}
-		return -1;
-	}
+	}, enumerable:false });
 
 })(window);
