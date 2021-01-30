f6e8607a936ec42c052c39ac0d06439c9d9ff800
元方 <luckeast@163.com>
2021-01-30 23:46:55 +0800

    update object.js. 1. [新增] Object.defineProperty兼容低版本浏览器

--- object.js
+++ object.js
@@ -1,17 +1,30 @@
 (function(win){
 
-	//浅复制对象所有成员到第一个参数的对象中
-	win.Object.assign = function(){
-		var args = arguments, arg0 = args[0];
-		for(var i = 1; i < args.length; i++){
-			var argI = args[i];
-			if(typeof(argI) == "object"){
-				for(var key in argI){
-					arg0[key] = argI[key];
+	if(!win.Object.assign){
+		//浅复制对象所有成员到第一个参数的对象中
+		win.Object.assign = function(){
+			var args = arguments, arg0 = args[0];
+			for(var i = 1; i < args.length; i++){
+				var argI = args[i];
+				if(typeof(argI) == "object"){
+					for(var key in argI){
+						arg0[key] = argI[key];
+					}
 				}
 			}
+			return arg0;
 		}
-		return arg0;
+
+		var defineProperty = win.Object.defineProperty || function(obj, prop, descriptor){
+			obj[prop] = descriptor.value;
+		};
+		win.Object.defineProperty = function(obj, prop, descriptor){
+			try{
+				defineProperty(obj, prop, descriptor);
+			} catch(e){ //IE8会报错误
+				obj[prop] = descriptor.value;
+			}
+		}
 	}
 
 })(window);
