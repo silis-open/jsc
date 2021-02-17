ecf440fe316ab7bbb99b012edcf59da2e49b41ef
元方 <luckeast@163.com>
2021-02-18 05:19:52 +0800

    update jsc.js. 1. [增加] cookie项的设置方法（setCookie） 2. [增加] cookie项的获取方法（getCookie） 3. [增加] cookie项的删除方法（removeCookie）

--- jsc.js
+++ jsc.js
@@ -118,17 +118,37 @@
 		}
 	}
 
+
+	win.setCookie = function(key, value, time){
+		if(time != null)
+			win.document.cookie = key + "=" + encodeURIComponent(value) + ";expires=" + new Date(new Date().getTime() + time).toGMTString();
+		else 
+			win.document.cookie = key + "=" + encodeURIComponent(value);
+	}
+
+	win.getCookie = function(key){
+		var matchs = win.document.cookie.match(key + "=([^;]*)");
+		if(matchs) return decodeURIComponent(matchs[1]); else return null;
+	}
+
+	win.removeCookie = function(key){
+		win.document.cookie = key + "=;expires=" + new Date(new Date().getTime() - 31536000000).toGMTString();
+	}
+
 	if(!win.localStorage){
 		win.localStorage = {
 			setItem:function(key,value){
-				win.document.cookie = "l_" + key + "=" + encodeURIComponent(value) + ";expires=" + new Date(new Date().getTime() + 31536000000).toGMTString();
+				win.setCookie("l_" + key, value, 31536000000);
+				//win.document.cookie = "l_" + key + "=" + encodeURIComponent(value) + ";expires=" + new Date(new Date().getTime() + 31536000000).toGMTString();
 			},
 			getItem:function(key){
-				var matchs = document.cookie.match("l_" + key + "=([^;]*)");
-				if(matchs) return decodeURIComponent(matchs[1]); else return null;
+				return win.getCookie("l_" + key);
+				//var matchs = win.document.cookie.match("l_" + key + "=([^;]*)");
+				//if(matchs) return decodeURIComponent(matchs[1]); else return null;
 			},
 			removeItem:function(key){
-				win.document.cookie = "l_" + key + "=;expires=" + new Date(new Date().getTime() - 31536000000).toGMTString();
+				win.removeCookie("l_" + key);
+				//win.document.cookie = "l_" + key + "=;expires=" + new Date(new Date().getTime() - 31536000000).toGMTString();
 			},
 			clear:function(){
 				var matchs = win.document.cookie.match(/l_[^=]+=/g);
@@ -146,14 +166,17 @@
 	if(!win.sessionStorage){
 		win.sessionStorage = {
 			setItem:function(key,value){
-				win.document.cookie = "s_" + key + "=" + encodeURIComponent(value);
+				win.setCookie("s_" + key, value);
+				//win.document.cookie = "s_" + key + "=" + encodeURIComponent(value);
 			},
 			getItem:function(key){
-				var matchs = document.cookie.match("s_" + key + "=([^;]*)");
-				if(matchs) return decodeURIComponent(matchs[1]); else return null;
+				return win.getCookie("s_" + key);
+				//var matchs = win.document.cookie.match("s_" + key + "=([^;]*)");
+				//if(matchs) return decodeURIComponent(matchs[1]); else return null;
 			},
 			removeItem:function(key){
-				win.document.cookie = "s_" + key + "=;expires=" + new Date(new Date().getTime() - 31536000000).toGMTString();
+				win.removeCookie("s_" + key);
+				//win.document.cookie = "s_" + key + "=;expires=" + new Date(new Date().getTime() - 31536000000).toGMTString();
 			},
 			clear:function(){
 				var matchs = win.document.cookie.match(/s_[^=]+=/g);
